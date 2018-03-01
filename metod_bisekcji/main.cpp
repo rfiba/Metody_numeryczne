7de6363628cf8c393f36ba98f4c0b14c1db0d217
rfiba <rafal.fiba@hotmail.com>
2018-03-01 14:08:42 +0100

    Pierwszy test: dodanie bisekcji i wyliczanie wartości funkcji, tylko dla funkcji kwadratowych.

--- metod_bisekcji/main.cpp
+++ metod_bisekcji/main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include <cmath>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+const double precision = 0.1;
+
+double calculateValueOfFunction(int aFactor, int bFactor, int cFactor, double point)
+{
+    return aFactor*pow(point,2) + bFactor*point + cFactor;
+}
+
+double doBisection(int aFactor, int bFactor, int cFactor, int beginOfInterval, int endOfInterval)
+{
+    double result = (beginOfInterval+endOfInterval)/2;
+    int valueOfResult = calculateValueOfFunction(aFactor,bFactor,cFactor,result);
+    if(valueOfResult <= precision)
+        return result;
+
+    if(calculateValueOfFunction(aFactor,bFactor,cFactor,beginOfInterval)*valueOfResult < 0)
+    {
+        doBisection(aFactor, bFactor, cFactor, beginOfInterval, result);
+    }
+    else if(valueOfResult*calculateValueOfFunction(aFactor,bFactor,cFactor,endOfInterval) < 0)
+    {
+        doBisection(aFactor, bFactor, cFactor, result, endOfInterval);
+    }
+
+}
+
+int main()
+{
+    int a,b,c, beginOfInterval, endOfInterval;
+    cout << "Get a, b and c factor" << endl;
+    cin >> a >> b >> c;
+    cout << "Get endpoints of interval" << endl;
+    cin >> beginOfInterval >> endOfInterval;
+    cout << doBisection(a,b,c,beginOfInterval, endOfInterval) << endl;
+
+
+
+
     return 0;
 }
