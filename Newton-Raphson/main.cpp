4ffbaa29e4f36f0dc5432db330723ef6cb713edb
rfiba <rafal.fiba@hotmail.com>
2018-03-03 09:29:47 +0100

    Newton-Raphson: pierwszy commit

---
+++ Newton-Raphson/main.cpp
@@ -0,0 +1,11 @@
+/*
+ Newton-Raphson
+ */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
---
+++ metod_bisekcji/cmake-build-debug/CMakeFiles/metod_bisekcji.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/metod_bisekcji.dir/main.cpp.o"
+  "metod_bisekcji.pdb"
+  "metod_bisekcji"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/metod_bisekcji.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
