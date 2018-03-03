2d10089e4a48dcc47bf744bbc9c077c15a196c06
rfiba <rafal.fiba@hotmail.com>
2018-03-03 09:41:57 +0100

    Newton-Raphson: dzialajaca wersja

--- Newton-Raphson/main.cpp
+++ Newton-Raphson/main.cpp
@@ -1,11 +1,33 @@
 /*
  Newton-Raphson
+ rfiba March 2018
  */
 #include <iostream>
-
+#include<cmath>
 using namespace std;
 
+
+const double precision = 0.000001;
+
+double calculateRoot(double root)
+{
+    double sideA = 1, sideB = root;
+
+    while(abs(sideA-sideB)>=precision)
+    {
+        sideA = (sideA+sideB)/2.;
+        sideB = root/sideA;
+    }
+
+    return sideA;
+
+}
+
 int main() {
-    
+    double root;
+    cout << "Get number" << endl;
+    cin >> root;
+    cout << calculateRoot(root) << endl;
+
     return 0;
 }
