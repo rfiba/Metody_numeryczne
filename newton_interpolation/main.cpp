c8a937e8d66e2677e7b61f327c2eeab8c035a706
rfiba <rafal.fiba@hotmail.com>
2018-03-24 00:04:46 +0100

    liczy tablice ilorazów różnicowych

--- newton_interpolation/main.cpp
+++ newton_interpolation/main.cpp
@@ -8,11 +8,6 @@ struct Point{
     double y;
 };
 
-double f(double x)
-{
-    return pow(x,3);//pow(x,4)*3 - pow(x,3) + pow(x,2)*2 -x +3;
-}
-
 double calculateDifferenceQuotient(int n, Point *arr)
 {
 
@@ -23,10 +18,10 @@ double calculateDifferenceQuotient(int n, Point *arr)
             if (i != j)
                 tmp *= arr[i].x - arr[j].x;
 
-
         result += arr[i].y / (tmp);
 
     }
+
     return result;
 }
 
@@ -40,10 +35,37 @@ double calculateNewtonInterpolation(int n, Point *arr, double x, int *difference
         tmp = 1.;
         for(int j = 0; j < i ; j++)
             tmp *= (x - arr[j].x);
-        
+
         result += tmp * differenceArr[i];
+
     }
+
     return result;
+}
+
+void createDifferentQuotientArray(int n, Point *arr)
+{
+    double **array = new double*[2*n-1];
+    for(int i = 0; i < 2*n-1; i++)
+        array[i] = new double[n];
+    //double array[5][5];
+
+    for(int i = 0, j = 0; i < 2*n-1; i+=2, j++)
+        array[i][0] = arr[j].y;
+
+
+    for(int i = 1, k = 2*n-1; i < n; i++, k--)
+    {
+        for(int j = i, t=i; j < k; j+=2, t++)
+        {
+            cout << "dla " << j << " " << i << endl;
+            printf("%f - %f / %f - %f", array[j+1][i-1], array[j-1][i-1], arr[t].x, arr[t-i].x);
+            cout << endl;
+            array[j][i] = (array[j+1][i-1]-array[j-1][i-1])/(arr[t].x-arr[t-i].x);
+            cout << array[j][i] << endl;
+        }
+    }
+    cout << "po" << endl;
 }
 
 int main() {
@@ -53,20 +75,13 @@ int main() {
     int *differenceArr = new int[n];
 
     for(int i = 0; i < n; i++)
-    {
         cin >> arr[i].x >> arr[i].y;
-        //arr[i].y = f(arr[i].x);
-    }
+    //for(int i = 0; i < n; i++)
+      //  differenceArr[i] = calculateDifferenceQuotient(i, arr);
 
-    for(int i = 0; i < n; i++)
-        differenceArr[i] = calculateDifferenceQuotient(i, arr);
-
-    for(int i = -5; i <= 5; i++)
-        cout << "f("<< i << ") = " << calculateNewtonInterpolation(5,arr,i, differenceArr) << endl;
-
-    cout << "real: " << endl;
-    for(int i =-5; i <= 5; i++ )
-        cout << "f(" << i << ") = " << f(i) << endl;
+    createDifferentQuotientArray(n,arr);
+    //for(int i = -5; i <= 5; i++)
+      //  cout << "f("<< i << ") = " << calculateNewtonInterpolation(n,arr,i, differenceArr) << endl;
 
     return 0;
 }
