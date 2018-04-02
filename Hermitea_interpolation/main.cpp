d7d27428fb89e7ce6f84c29433235256eda38d05
rfiba <rafal.fiba@hotmail.com>
2018-04-02 11:00:01 +0200

    W pełni działa liczy całą tabelkę

--- Hermitea_interpolation/main.cpp
+++ Hermitea_interpolation/main.cpp
@@ -2,6 +2,52 @@
 
 using namespace std;
 
+int factorial(int k)
+{
+    if( k < 2)
+        return 1;
+
+    return k*factorial(k-1);
+}
+
+double** createDifferentQuotientArray(int n,int * arrD, double *arrX, double *arrY, bool i) {
+    double **array = new double *[n];
+    for (int i = 0; i < n; i++) {
+        array[i] = new double[n];
+        array[i][0] = arrY[i-arrD[i]];
+    }
+
+    for (int i = 1; i < n; i++){
+        for (int j = i; j < n; j++) {
+            if((arrX[j] - arrX[j - i]) == 0)
+                array[j][i] = (arrY[j - arrD[j - i]]) / factorial(i);
+            else
+                array[j][i] = (array[j][i - 1] - array[j - 1][i - 1]) / (arrX[j] - arrX[j - i]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 1; j <= i; j++)
+            cout << " " << array[i][j];
+        cout << endl;
+    }
+
+        return array;
+
+}
+
+void prepareDerivativesArr(int * arrD, double * arrX, int n)
+{
+    arrD[0] = 0;
+    for(int i = 1; i < n; i++)
+    {
+        if(arrX[i] == arrX[i-1])
+            arrD[i] = arrD[i-1] + 1;
+        else
+            arrD[i] = 0;
+    }
+}
+
 int main() {
     int n;
     cin >> n;
@@ -22,6 +68,8 @@ int main() {
             arrD[i] = 0;
     }
 
+    createDifferentQuotientArray(n, arrD, arrX, arrY, 1);
+
     for(int j = 1; j < n; j++)
     {
         for(int i = n - 1; i >= j; i--)
@@ -34,6 +82,8 @@ int main() {
             }
         }
     }
+
+
 
     for(int i = 0; i < n; i++)
         cout << arrY[i] << endl;
