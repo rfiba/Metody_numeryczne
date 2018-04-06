e5272c9e8e5c557d3455d6d161bc215f4682e515
rfiba <rafal.fiba@hotmail.com>
2018-04-06 16:26:44 +0200

    doPolynomial() - działa

--- Monte_Carlo_method/main.cpp
+++ Monte_Carlo_method/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double doPolynomial(int degreeOfPolynomial, double * coefficientArr, double x)
+{
+    double result = coefficientArr[0];
+
+    for(int i = 1; i <= degreeOfPolynomial; i++)
+        result += coefficientArr[i]*pow(x,i);
+
+    return result;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int degreeOfPolynomial;
+    double minimumInRange, maximumInRange, beginOfRange, endOfRange;
+    cin >> degreeOfPolynomial >> minimumInRange >> maximumInRange >> beginOfRange >> endOfRange;
+
+    double *coefficientArr = new double[degreeOfPolynomial+1];
+
+    for (int i = 0; i <= degreeOfPolynomial; i++)
+        cin >> coefficientArr[i];
+
+    cout << doPolynomial(degreeOfPolynomial, coefficientArr, 5) << endl;
+
     return 0;
 }
