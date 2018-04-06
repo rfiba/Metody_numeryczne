334181af4cd3df66a9498eea9aa75e776c0f40f1
rfiba <rafal.fiba@hotmail.com>
2018-04-06 11:57:17 +0200

    Pierwszy commit

---
+++ Least_squares_approximation/cmake-build-debug/CMakeFiles/Least_squares_approximation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Least_squares_approximation.dir/main.cpp.o"
+  "Least_squares_approximation.pdb"
+  "Least_squares_approximation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Least_squares_approximation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Least_squares_approximation/cmake-build-debug/CMakeFiles/Least_squares_approximation.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Least_squares_approximation.
+# This may be replaced when dependencies are built.
