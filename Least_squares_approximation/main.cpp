d949b096af41d416b3870c28746e63c975080e75
rfiba <rafal.fiba@hotmail.com>
2018-04-06 13:13:37 +0200

    Pełna funcjonalność - dział

--- Least_squares_approximation/main.cpp
+++ Least_squares_approximation/main.cpp
@@ -1,8 +1,61 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+double sumOfArrayXPower(int numberOfPoints, double *arrX, double power)
+{
+    double result = 0;
+    if(power > 1){
+        for(int i = 0; i < numberOfPoints; i++)
+            result += pow(arrX[i], power);
+    }
+    else
+    {
+        if(power == 0)
+            return numberOfPoints;
 
+        for(int i = 0; i < numberOfPoints; i++)
+            result += arrX[i];
+    }
+    return result;
+}
+
+double sumOfArrayYPower(int numberOfPoints, double *arrX, double *arrY, double power)
+{
+    double result = 0;
+    if(power > 1){
+        for(int i = 0; i < numberOfPoints; i++)
+            result += arrY[i]*pow(arrX[i], power);
+    }
+    else
+    {
+        if(!power){
+            for(int i = 0; i < numberOfPoints; i++)
+                result += arrY[i];
+        }
+        else
+        {
+            for(int i = 0; i < numberOfPoints; i++)
+                result += arrY[i]*arrX[i];
+        }
+
+    }
+    return result;
+}
+
+void do_Lest_Squares_Equations(double *arrX, double *arrY, double **resultArr, int numberOfPoints, int degreeOfPolynomial)
+{
+
+    for(int i = 0; i <= degreeOfPolynomial; i++)
+    {
+        for(int j = 0; j <= degreeOfPolynomial; j++)
+            resultArr[i][j] = sumOfArrayXPower(numberOfPoints, arrX, j+i);
+    }
+
+    for(int i = 0; i <= degreeOfPolynomial; i++)
+        resultArr[i][degreeOfPolynomial+1] = sumOfArrayYPower(numberOfPoints, arrX, arrY, i);
+}
 
 int main() {
     int numberOfPoints, degreeOfPolynomial;
@@ -11,16 +64,26 @@ int main() {
     double *arrX, *arrY, **resultArr;
     arrX = new double[numberOfPoints];
     arrY = new double[numberOfPoints];
-    resultArr = new double*[numberOfPoints+1];
-    for(int i = 0; i <= numberOfPoints ; i++)
-    {
-        resultArr[i] = new double[numberOfPoints+1];
-    }
+    resultArr = new double*[degreeOfPolynomial+1];
+    for(int i = 0; i <= degreeOfPolynomial ; i++)
+        resultArr[i] = new double[degreeOfPolynomial+2];
 
     for(int i = 0; i < numberOfPoints; i++)
         cin >> arrX[i] >> arrY[i];
 
+    do_Lest_Squares_Equations(arrX, arrY, resultArr, numberOfPoints, degreeOfPolynomial);
+    for(int i = 0; i <= degreeOfPolynomial; i++)
+    {
+        for(int j = 0; j <= degreeOfPolynomial; j++)
+            cout << "a" << j << " " << resultArr[i][j] << " ";
+
+        cout << "= " << resultArr[i][degreeOfPolynomial+1] << endl;
+    }
+
     delete []arrX;
     delete []arrY;
+    for(int i = 0; i <= degreeOfPolynomial; i++)
+        delete []resultArr[i];
+    delete []resultArr;
     return 0;
 }
