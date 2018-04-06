4a16ecc440012ef6facc68ca37bc949c16640205
rfiba <rafal.fiba@hotmail.com>
2018-04-06 16:11:46 +0200

    Pierwszy commit

---
+++ Monte_Carlo_method/cmake-build-debug/CMakeFiles/Monte_Carlo_method.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Monte_Carlo_method.dir/main.cpp.o"
+  "Monte_Carlo_method.pdb"
+  "Monte_Carlo_method"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Monte_Carlo_method.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Monte_Carlo_method/cmake-build-debug/CMakeFiles/Monte_Carlo_method.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Monte_Carlo_method.
+# This may be replaced when dependencies are built.
