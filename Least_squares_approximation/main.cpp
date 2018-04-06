8a8a10da8f4418f675684286e9bc5e8a8f58ddd5
rfiba <rafal.fiba@hotmail.com>
2018-04-06 12:17:26 +0200

    I/O i inicjalizacja tablic

--- Least_squares_approximation/main.cpp
+++ Least_squares_approximation/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+using namespace std;
+
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int numberOfPoints, degreeOfPolynomial;
+    cin >> numberOfPoints >> degreeOfPolynomial;
+
+    double *arrX, *arrY, **resultArr;
+    arrX = new double[numberOfPoints];
+    arrY = new double[numberOfPoints];
+    resultArr = new double*[numberOfPoints+1];
+    for(int i = 0; i <= numberOfPoints ; i++)
+    {
+        resultArr[i] = new double[numberOfPoints+1];
+    }
+
+    for(int i = 0; i < numberOfPoints; i++)
+        cin >> arrX[i] >> arrY[i];
+
+    delete []arrX;
+    delete []arrY;
     return 0;
 }
