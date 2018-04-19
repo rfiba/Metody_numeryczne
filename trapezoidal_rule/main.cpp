bedbe2d8fc740620da48215b5361c7f29861c7d8
rfiba <rafal.fiba@hotmail.com>
2018-04-19 13:00:15 +0200

    Pierwszy commit

--- trapezoidal_rule/main.cpp
+++ trapezoidal_rule/main.cpp
@@ -1,6 +1,29 @@
+/*
+ * @author: rfiba April 2018
+ * Trapeizoidal Rule
+ */
 #include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double calculatePolynomial(int degreeOfPolynomial, double * coefficientArr, double x)
+{
+    double result = coefficientArr[0];
+
+    for(int i = 1; i <= degreeOfPolynomial; i++)
+        result += coefficientArr[i]*pow(x,i);
+
+    return result;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int degreeOfPolynomial, numberOfPoints, numberOfTrapezoids;
+
+    double *coefficientArr = new double[degreeOfPolynomial+1];
+
+    for (int i = 0; i <= degreeOfPolynomial; i++)
+        cin >> coefficientArr[i];
+
     return 0;
 }
