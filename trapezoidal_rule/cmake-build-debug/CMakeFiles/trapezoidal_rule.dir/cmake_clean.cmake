7093cfe2c1e665d7f8175e5c2d7a2806e0d1b57d
rfiba <rafal.fiba@hotmail.com>
2018-04-19 11:55:38 +0200

    pierwszy commit

---
+++ trapezoidal_rule/cmake-build-debug/CMakeFiles/trapezoidal_rule.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trapezoidal_rule.dir/main.cpp.o"
+  "trapezoidal_rule.pdb"
+  "trapezoidal_rule"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trapezoidal_rule.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ trapezoidal_rule/cmake-build-debug/CMakeFiles/trapezoidal_rule.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for trapezoidal_rule.
+# This may be replaced when dependencies are built.
