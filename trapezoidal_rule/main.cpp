72fe8ce6d0f1c9a0a6db6575fb22911b20a23899
rfiba <rafal.fiba@hotmail.com>
2018-04-20 12:16:32 +0200

    Dodana funkcja calculateAreaOfTrapezoid() i calculateAre()

--- trapezoidal_rule/main.cpp
+++ trapezoidal_rule/main.cpp
@@ -17,13 +17,33 @@ double calculatePolynomial(int degreeOfPolynomial, double * coefficientArr, doub
     return result;
 }
 
+double calculateAreaOfTrapezoid(double aBase, double bBase, double height)
+{
+    return ((aBase+bBase)*height)/2;
+}
+
+double calculateArea(int degreeOfPolynomial, int numberOfTrapezoids, int beginOfRange, int endOfRange, double * coefficientArr)
+{
+    double legOfTrapezoid = (endOfRange-beginOfRange)/numberOfTrapezoids;
+    double result = 0, beginPoint = beginOfRange;
+    for(int i = 0; i < numberOfTrapezoids;i++)
+    {
+        result += calculateAreaOfTrapezoid(calculatePolynomial(degreeOfPolynomial, coefficientArr, beginPoint),
+        calculatePolynomial(degreeOfPolynomial, coefficientArr, beginPoint+legOfTrapezoid),
+        legOfTrapezoid);
+    }
+    return result;
+}
+
 int main() {
-    int degreeOfPolynomial, numberOfPoints, numberOfTrapezoids;
+    int degreeOfPolynomial, numberOfPoints, numberOfTrapezoids, beginOfRange, endOfRange;
 
     double *coefficientArr = new double[degreeOfPolynomial+1];
+    cin >> degreeOfPolynomial >> numberOfTrapezoids >> beginOfRange >> endOfRange;
 
     for (int i = 0; i <= degreeOfPolynomial; i++)
         cin >> coefficientArr[i];
 
+    cout << calculateArea(degreeOfPolynomial, numberOfTrapezoids, beginOfRange, endOfRange, coefficientArr) << endl;
     return 0;
 }
