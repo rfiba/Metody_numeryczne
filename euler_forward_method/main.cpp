453ec6acb66030be708cc545c15357ddc20a0dc1
rfiba <rafal.fiba@hotmail.com>
2018-05-04 21:37:05 +0200

    Function calculateEulerMethod() - prepared

--- euler_forward_method/main.cpp
+++ euler_forward_method/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 
+using namespace std;
 
+double calculataFunction(double t, double x)
+{
+    return 0;
+}
 
+void calculateEulerMethod(double xMin, double xMax, int numberOfPoints, double valueOfXMin, double *resultArr)
+{
+    double step = (xMax-xMin)/numberOfPoints;
+    resultArr[0] = valueOfXMin;
+    double t = xMin, tPrevious = xMin;
+
+    for(int i = 1; i < numberOfPoints; i++)
+    {
+        resultArr[i] = resultArr[i-1] + step*calculataFunction(tPrevious, resultArr[i-1]);
+        tPrevious = t;
+        t = xMin +(i+1)*step;
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int numberOfPoints;
+    double xMin, xMax, valueOfXMin;
+    cin >> numberOfPoints >> xMin >> xMax >> valueOfXMin;
+    double * resultArr = new double[numberOfPoints];
+    
+    delete resultArr;
     return 0;
 }
