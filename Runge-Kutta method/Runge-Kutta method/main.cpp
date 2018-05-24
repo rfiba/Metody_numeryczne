dd90a96d3bdc6f1a1deb24a697728c3b2881c77b
rfiba <rafal.fiba@hotmail.com>
2018-05-24 15:58:25 +0200

    Initial commit

---
+++ Runge-Kutta method/Runge-Kutta method/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+double calcuateFunction(double x, double y)
+{
+	return 0;
+}
+
+double 
+
+int main() {
+	double x0, y0, step;
+	int numberOfPoints;
+	int *result = new int[numberOfPoints];
+	cin >> numberOfPoints >> x0 >> y0 >> step;
+
+	delete numberOfPoints;
+	return 0;
+}
