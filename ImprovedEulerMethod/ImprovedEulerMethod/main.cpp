4e52d51944af675207662836503db9491a79ee65
rfiba <rafal.fiba@hotmail.com>
2018-05-24 17:36:12 +0200

    calculateImprovedEuler() implemented

--- ImprovedEulerMethod/ImprovedEulerMethod/main.cpp
+++ ImprovedEulerMethod/ImprovedEulerMethod/main.cpp
@@ -2,6 +2,37 @@
 
 using namespace std;
 
+struct Point {
+
+	double x;
+	double y;
+	Point() {};
+	Point(double xToAdd, double yToAdd) {
+		x = xToAdd;
+		y = yToAdd;
+	}
+	Point operator+(Point toAdd) {
+		return Point(x + toAdd.x, y + toAdd.y);
+	}
+};
+
+double calculateFunction(Point toCalculate)
+{
+	return toCalculate.x + toCalculate.y;
+}
+
+void calculateImprovedEuler(Point initialPoint, double step, int numberOfPoints, Point * result)
+{
+	double forwardEulerResult, backwardEulerResult;
+	for (int i = 1; i <= numberOfPoints; i++)
+	{
+		result[i].x = result[i - 1].x + step;
+		forwardEulerResult = result[i - 1].y + step*calculateFunction(result[i - 1]);
+		backwardEulerResult = result[i - 1].y + step*calculateFunction(Point(result[i - 1].x, forwardEulerResult));
+		result[i].y = (forwardEulerResult + backwardEulerResult) / 2;
+	}
+}
+
 int main()
 {
 	return 0;
