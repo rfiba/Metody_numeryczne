519241753c9897b72e5ae9523a67314d8f7a1127
rfiba <rafal.fiba@hotmail.com>
2018-05-24 16:45:15 +0200

    Works fine

--- Runge-Kutta method/Runge-Kutta method/main.cpp
+++ Runge-Kutta method/Runge-Kutta method/main.cpp
@@ -1,23 +1,46 @@
+/*
+	@author: rfiba May 2018
+	classical Runge–Kutta method (RK4)
+	Input:
+	[numberOfPoints] [initialX] [initialY] [step]
+	Output:
+	Points from initial to initial + numberOfPoints
+*/
+
 #include <iostream>
 
 using namespace std;
 
 struct Point {
+
 	double x;
 	double y;
+	Point() {};
+	Point(double xToAdd, double yToAdd) {
+		x = xToAdd;
+		y = yToAdd;
+	}
+	Point operator+(Point toAdd) {
+		return Point(x + toAdd.x, y + toAdd.y);
+	}
 };
 
 double calcuateFunction(Point toCalculate)
 {
-	return 0;
+	return toCalculate.x + toCalculate.y;
 }
 
-double calculateRungeKuttaMethod(Point initiaPoint, double step, int numberOfPoints, double * result)
+void calculateRungeKuttaMethod(Point initialPoint, double step, int numberOfPoints, Point * result)
 {
 	double k1, k2, k3, k4;
-	for (int i = 1; i < numberOfPoints; i++)
+	for (int i = 1; i <= numberOfPoints; i++)
 	{
-		k1 = step*calcuateFunction()
+		k1 = step*calcuateFunction(result[i - 1]);
+		k2 = step*calcuateFunction(result[i - 1] + Point(0.5*step, k1/2));
+		k3 = step*calcuateFunction(result[i - 1] + Point(0.5*step, k2/2));
+		k4 = step*calcuateFunction(result[i - 1] + Point(step, k3));
+		result[i].y = result[i - 1].y + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+		result[i].x = result[i - 1].x + step;
 	}
 }
 
@@ -25,10 +48,17 @@ int main() {
 	double step;
 	Point initialPoint;
 	int numberOfPoints;
-	Point *result = new Point[numberOfPoints+1];
-	
+
 	cin >> numberOfPoints >> initialPoint.x >> initialPoint.y >> step;
+	Point *result = new Point[numberOfPoints + 1];
 	result[0] = initialPoint;
+
+	calculateRungeKuttaMethod(initialPoint, step, numberOfPoints, result);
+	
+	for (int i = 0; i <= numberOfPoints;i++)
+		cout << result[i].x << " " << result[i].y << endl;
+
+	system("pause");
 	delete result;
 	return 0;
 }
